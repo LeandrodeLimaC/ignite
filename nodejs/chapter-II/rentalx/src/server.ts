a57ca00367b4d45ffc4763d17c4d82eec6931bed
LeandrodeLimaC <leandrolima7472@gmail.com>
2022-12-29 19:44:53 -0300

    feat(Categories): registration route created

--- nodejs/chapter-II/rentalx/src/server.ts
+++ nodejs/chapter-II/rentalx/src/server.ts
@@ -1,16 +1,14 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
+
 app.use(express.json());
+app.use(categoriesRoutes);
 
 app.get("/", (request, response) => {
   response.json({ message: "Hello world" });
-});
-
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-
-  response.json({ name });
 });
 
 app.listen("3333", () => console.log("Server is running!"));
