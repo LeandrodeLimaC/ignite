0561b082cebf9cc13090d968495dceb291067811
LeandrodeLimaC <leandrolima7472@gmail.com>
2023-01-02 19:31:10 -0300

    refactor(Category): created useCases

--- nodejs/chapter-II/rentalx/src/routes/categories.routes.ts
+++ nodejs/chapter-II/rentalx/src/routes/categories.routes.ts
@@ -1,18 +1,13 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategories";
 
 const categoriesRoutes = Router();
 const categoryRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoryRepository);
-  createCategoryService.execute({ description, name });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
