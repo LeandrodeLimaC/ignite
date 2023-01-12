9426ffcf71ccbf09c8d71a5961d397022ca45737
LeandrodeLimaC <leandrolima7472@gmail.com>
2023-01-12 09:44:29 -0300

    first commit docker file created

---
+++ nodejs/chapter-III/rentalx/src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -0,0 +1,57 @@
+import csvParser from "csv-parser";
+import fs from "fs";
+
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoriesUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  private loadCategories(
+    file: Express.Multer.File
+  ): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const parseFile = csvParser();
+
+      const categories: IImportCategory[] = [];
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on("error", (error) => reject(error));
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.map(async (category) => {
+      const { name } = category;
+
+      const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+      if (!categoryAlreadyExists) {
+        this.categoriesRepository.create(category);
+      }
+    });
+  }
+}
+
+export { ImportCategoriesUseCase };
