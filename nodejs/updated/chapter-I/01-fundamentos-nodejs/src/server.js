a9a64a0a92aac0998c7added49cb91087612e994
LeandrodeLimaC <leandrolima7472@gmail.com>
2023-01-19 19:40:21 -0300

    basic understanding about status code

--- nodejs/updated/chapter-I/01-fundamentos-nodejs/src/server.js
+++ nodejs/updated/chapter-I/01-fundamentos-nodejs/src/server.js
@@ -1,5 +1,35 @@
 import http from 'node:http'
 
+// - Criar usuários 
+// - Listar usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET => Buscar um recurso
+// POST => Criar um recurso
+// PUT => Atualizar um recurso
+// PATCH => Atualizar uma informação específica de um recurso
+// DELETE => Deletar um recurso
+
+// GET /users => Buscar usuários
+// POST /users => Criar usuário
+
+// Stateful vs Stateless
+//  - Stateful diz respeito a aplicação manter os dados em memória
+//    de modo que, caso a aplicação seja reiniciada, ela se 
+//    comportará de modo diferente
+//  
+//  - Stateless, o estado é persistido e mantido em um ambiente externo
+//   
+
+// Cabeçalhos (Requisição/Resposta) => Metadados
+
+
+
 const users = []
 
 const server = http.createServer((req, res) => {
@@ -13,14 +43,15 @@ const server = http.createServer((req, res) => {
 
   if (method === 'POST' & url === '/users') {
     users.push({
+      id: "1",
       name: "John Doe",
       email: "john@example.com"
     })
 
-    return res.end('Criação de usuário')
+    return res.writeHead(201).end()
   }
 
-  return res.end('Hello world!')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
