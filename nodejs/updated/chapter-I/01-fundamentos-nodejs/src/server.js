21d145ceb6b7efc3029862ecaad18dbd46d907a8
LeandrodeLimaC <leandrolima7472@gmail.com>
2023-01-19 19:21:33 -0300

    basic understanding abou headers

--- nodejs/updated/chapter-I/01-fundamentos-nodejs/src/server.js
+++ nodejs/updated/chapter-I/01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,25 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' & url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' & url === '/users') {
+    users.push({
+      name: "John Doe",
+      email: "john@example.com"
+    })
+
+    return res.end('Criação de usuário')
+  }
+
   return res.end('Hello world!')
 })
 
