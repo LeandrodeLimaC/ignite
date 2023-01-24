067bc81e267bb355de472940db4101ecfd491e92
LeandrodeLimaC <leandrolima7472@gmail.com>
2023-01-24 20:20:47 -0300

    chore: info about streams, added transform and writable streams

--- nodejs/updated/chapter-I/01-fundamentos-nodejs/streams/fundamentos.js
+++ nodejs/updated/chapter-I/01-fundamentos-nodejs/streams/fundamentos.js
@@ -1,18 +1,24 @@
-// Todas as entradas e saidas do node são streams
+/**
+ * ** Streams ** 
+ * Uma abertura de conexão, de forma que possa ser enviada pedaços
+ * de informações aos poucos
+ * 
+ * - Todas as portas de entradas e saidas do node são automaticamente
+ *   streams
+ * - Pedaços (Chunks) nunca poderão estar em tipos 
+ *   primitivos, apenas em Buffers
+ * - Existe um tipo `Duplex` que pode ter tanto o 
+ *   metodo de leitura quando de escrita
+ */
 
-// Streams -> 
-//   Pedaços nunca poderão estar em tipos primitivos e sim em Buffers
-//   Buffer.from()
+import { Readable, Writable, Transform } from 'node:stream'
 
-/* 
-// Readable stream
-process.stdin
-  // Writable stream
-  .pipe(proccess.stdout) 
-*/
-
-import { stdout } from 'node:process'
-import { Readable } from 'node:stream'
+/**
+ * ** Stream de leitura **
+ * Tem como proposito, enviar dados e fornecer informações
+ * IMPORTANTE: Metodo obrigatório _read()
+ * ex .: stdin,
+ */
 
 class OneToHundred extends Readable {
   index = 1
@@ -32,6 +38,45 @@ class OneToHundred extends Readable {
   }
 }
 
+/***
+ * ** Stream de transformação **
+ * Obrigatóriamente precisa ler dados de um lugar
+ * e escrever dados para outro lugar
+ * ela é uma stream utilizada no intermeio, para comunicação
+ * de outras duas streams
+ * IMPORTANTE: Sempre utilizar para processar dados
+ */
+
+class InverseNumberStream extends Transform {
+  _transform(buffer, encode, callback) {
+    const result = Number(buffer.toString()) * -1
+
+    /**
+     * Recebe 2 parametros
+     * - Erro
+     * - Resultado
+     */
+    callback(null, Buffer.from(String(result)))
+  }
+}
+
+/**
+ * ** Stream de escrita **
+ * É responsavel por receber dados de uma stream de leitura e 
+ * posteriormente realizar alguma ação com estes dados
+ * IMPORTANTE: NUNCA transforma dados, apenas os processa!
+ * IMPORTANTE: NUNCA retorna nada!
+ * ex .: stdout
+ */
+
+class MultiplyByTenStream extends Writable {
+  _write(buffer, encode, callback){
+    console.log(Number(buffer.toString()) * 10)
+
+    callback()
+  }
+}
+
 new OneToHundred()
-  .pipe(stdout)
-  
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
