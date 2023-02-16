8da6883b62bc672930a43f5c64612791ccf4082e
LeandrodeLimaC <leandrolima7472@gmail.com>
2023-02-16 10:17:47 -0300

    feat(Header): created component

--- reactjs/01-fundamentos-reactjs/src/App.jsx
+++ reactjs/01-fundamentos-reactjs/src/App.jsx
@@ -1,6 +1,12 @@
+import { Header } from "./components/Header"
+import "./global.css"
+
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <Header />
+      <h1>Hello world</h1>
+    </>
   )
 }
 
