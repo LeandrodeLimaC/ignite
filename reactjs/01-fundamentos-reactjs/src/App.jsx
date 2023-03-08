cf482d14936ca2238f02c2c3c4e02c5ee9fba602
LeandrodeLimaC <leandrolima7472@gmail.com>
2023-03-08 09:43:56 -0300

    Os motores do React - Propriedades do Post

--- reactjs/01-fundamentos-reactjs/src/App.jsx
+++ reactjs/01-fundamentos-reactjs/src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css'
 
 import "./global.css"
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/leandrodelimac.png',
+      name: 'Leandro de Lima',
+      role: 'Front-end @ Alliar'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'paragraph', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-03-08 09:04:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/sarahmelo.png',
+      name: 'Sarah Melo',
+      role: 'Front-end @ IPMedia'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'paragraph', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-03-06 22:38:00')
+  }
+]
+
 function App() {
   return (
     <div>
@@ -14,7 +45,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post></Post>
+          {
+            posts.map((post) => (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            ))
+          }
         </main>
       </div>
     </div>
