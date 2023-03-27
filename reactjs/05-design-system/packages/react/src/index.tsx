4550485b375cde0cef6fef82052e80884ca26bfd
LeandrodeLimaC <leandrolima7472@gmail.com>
2023-03-27 11:02:18 -0300

    Design System em React - Story: Button

--- reactjs/05-design-system/packages/react/src/index.tsx
+++ reactjs/05-design-system/packages/react/src/index.tsx
@@ -1,11 +1,34 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$ignite500',
   borderRadius: '$md',
+  padding: '$2 $4',
+  border: 0,
+  color: '$white',
+  fontSize: '$md',
+  fontWeight: '$bold',
+  cursor: 'pointer',
+  transition: 'all 0.2s ease-in-out',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$2 $4',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Hello world</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
