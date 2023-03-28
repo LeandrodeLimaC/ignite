1083be10302c4670f6079c4b2d526b06afc29a72
LeandrodeLimaC <leandrolima7472@gmail.com>
2023-03-28 17:21:33 -0300

    Componentes do Design System - Componente: TextInput

---
+++ reactjs/05-design-system/packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,41 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+  args: {
+    children: 'Lorem Ipsum dolor sit amet, consectetur adipiscing elit',
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text>Email address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name here',
+  },
+}
+
+export const Prefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
